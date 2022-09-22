ab13a714eab5f64b2ce53e0eb870ba14b42eec73
jbep <joshuabemartin@gmail.com>
2022-09-21 20:29:10 -0400

    update

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+/* == Express Instance == */
 const express = require ("express")
 
 const app = express()
+/* == Internal Modules == */
 const routes = require('./routes')
+
+/* == Port == */
 const PORT = process.env.PORT || 3001;
 
-/* == Internal Modules == */
 
 
+/* == Middleware == */
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+/* == Routes == */
 app.use("/pins", routes.pins)
 
 app.listen (PORT, () => {
