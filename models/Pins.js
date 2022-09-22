ebf42ed0773d651eff9b0622f5b20880f95bfdc7
jbep <joshuabemartin@gmail.com>
2022-09-21 21:56:41 -0400

    pins model setup

--- models/Pins.js
+++ models/Pins.js
@@ -6,7 +6,7 @@ const pinSchema = new mongoose.Schema({
   description: { type: String, required: true },
   rating: { type: Number, required: true, min: 0, max:5},
   longitude: { type: Number, required: true },
-  Latitude: { type: Number, required: true },
+  latitude: { type: Number, required: true },
   image: {type: Buffer, required: true},
   imageType: {type: String, required: true},
 })
