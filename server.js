5da4378e13ef640d892c48c3489ac09bfa3ad4c1
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-09-27 19:11:57 -0500

    core added

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const routes = require('./routes')
 /* == Bcrypt == */
 const bcrypt = require('bcrypt')
 
+//import cors
+const cors = require('cors')
+// whitelist & corsOptions
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
 require('dotenv').config()
 
 /* == Port == */
@@ -17,6 +31,7 @@ app.use("/pins", routes.pins)
 app.use("/users", routes.users)
 
 /* == Middleware == */
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
